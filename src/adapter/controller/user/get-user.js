fc6767d7a636856dba7c600fa3e6c7610241943b
Odilomar <odilomarjunior.contact@gmail.com>
2021-08-23 20:51:49 -0300

    fix(findUser): return 404 when user don´t exists

--- src/adapter/controller/user/get-user.js
+++ src/adapter/controller/user/get-user.js
@@ -2,13 +2,17 @@ module.exports = function buildGetUser({ findUser }) {
   return async function getUser(request, response) {
     try {
       const id = request.params.id;
-      const users = await findUser({ id });
+      const user = await findUser({ id });
 
-      return response.json(users);
+      if (!user) {
+        return response.status(404).json({ message: "User don´t exists!" });
+      }
+
+      return response.json(user);
     } catch (e) {
       console.log(e);
 
-      return response.statusCode(400).json({ error: e.message });
+      return response.status(400).json({ error: e.message });
     }
   };
 };
